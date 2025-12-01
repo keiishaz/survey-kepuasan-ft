2bcb9c5cfec2c6f4381dfc48169e97a3fcbc9458
keiishaz <keishzaa124@gmail.com>
2025-12-01 07:34:47 +0700

    perbaikan kecil pada teks yang tidak muncul

--- resources/views/Admin/Form/form.blade.php
+++ resources/views/Admin/Form/form.blade.php
@@ -168,7 +168,7 @@
                                             <svg class="w-3 h-3 mr-1 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
                                             </svg>
-                                            {{ $form->respondens_count }}
+                                            {{ $form->respondens_count }} responden
                                         </span>
                                     </div>
                                     <div class="flex justify-between">
@@ -265,7 +265,7 @@
                                                 <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
                                                 </svg>
-                                                <span class="text-xs font-medium">{{ $form->respondens_count }}</span>
+                                                <span class="text-xs font-medium">{{ $form->respondens_count }} Responden</span>
                                             </div>
                                         </td>
                                         <td class="px-4 py-3">
